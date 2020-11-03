f65d7054b6edb54b3cf62fc9aaee557a1b5b08ba
Sachin <42708057+SachinBuddy@users.noreply.github.com>
2020-11-03 15:43:27 +0530

    Add files via upload

---
+++ create_data.py
@@ -0,0 +1,64 @@
+#creating database
+import cv2, sys, numpy, os
+import urllib
+import numpy as np
+haar_file = 'haarcascade_frontalface_default.xml'
+datasets = 'datasets'  #All the faces data will be present this folder
+namas=input("Enter the Name : ")
+sub_data = namas
+####sub_data = 'hai'  #These are sub data sets of folder, for my faces I've used my name
+
+path = os.path.join(datasets, sub_data)
+if not os.path.isdir(path):
+    os.mkdir(path)
+(width, height) = (130, 100)    # defining the size of images 
+
+
+face_cascade = cv2.CascadeClassifier(haar_file)
+webcam = cv2.VideoCapture(0) #'0' is use for my webcam, if you've any other camera attached use '1' like this
+##url="http://192.168.43.1:8080/shot.jpg"
+# The program loops until it has 30 images of the face.
+count = 1
+while count < 30: 
+    (_, im) = webcam.read()
+##    imgPath=urllib.urlopen(url)
+##    imgNp=np.array(bytearray(imgPath.read()),dtype=np.uint8)
+##    im=cv2.imdecode(imgNp,-1)
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 4)
+    ospath=("C:/Users/Sachin Sehwag/Desktop/Multiplefacedetectionattendanceandwritingittoa file/datasets/",namas,"/",count,".png")
+    for (x,y,w,h) in faces:
+        cv2.rectangle(im,(x,y),(x+w,y+h),(255,0,0),2)
+        face = gray[y:y + h, x:x + w]
+        face_resize = cv2.resize(face, (width, height))
+        cv2.imwrite('%s/%s.png' % (path,count), face_resize)
+    #if(os.path.isfile(ospath)):
+    count += 1
+	
+    cv2.imshow('OpenCV', im)
+    key = cv2.waitKey(10)
+    if key == 27:
+        break
+count = 31
+temp = input("Press any key and enter (for Specs)")
+while count < 60: 
+    (_, im) = webcam.read()
+##    imgPath=urllib.urlopen(url)
+##    imgNp=np.array(bytearray(imgPath.read()),dtype=np.uint8)
+##    im=cv2.imdecode(imgNp,-1)
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 4)
+    ospath=("C:/Users/Sachin Sehwag/Desktop/Multiplefacedetectionattendanceandwritingittoa file/datasets/",namas,"/",count,".png")
+    for (x,y,w,h) in faces:
+        cv2.rectangle(im,(x,y),(x+w,y+h),(255,0,0),2)
+        face = gray[y:y + h, x:x + w]
+        face_resize = cv2.resize(face, (width, height))
+        cv2.imwrite('%s/%s.png' % (path,count), face_resize)
+    #if(os.path.isfile(ospath)):
+    count += 1
+	
+    cv2.imshow('OpenCV', im)
+    key = cv2.waitKey(10)
+    if key == 27:
+        break
+
